8b2d59e3c4742a587dd56e2ab03bec966bb459d2
3Mydlo3 <mydlo333@interia.pl>
2024-12-15 21:44:42 +0100

    Remove UI includes

--- addons/common/script_component.hpp
+++ addons/common/script_component.hpp
@@ -1,10 +1,6 @@
 #define COMPONENT common
 #include "\z\NEW_MOD_ABBR\addons\main\script_mod.hpp"
 
-#include "\a3\ui_f\hpp\defineCommonGrids.inc"
-#include "\a3\ui_f\hpp\defineResincl.inc"
-#include "\a3\ui_f\hpp\defineResinclDesign.inc"
-
 // #define DEBUG_MODE_FULL
 // #define DISABLE_COMPILE_CACHE
 
